80935a712224d507ed621ecd81426101efda2bc0
Ali Sharafi <47324261+ali-sharafi@users.noreply.github.com>
2023-06-16 21:53:59 +0000

    Add chat & message Id to env file

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ const app = express();
 const server = http.createServer(app);
 const serverPort = process.env.SERVER_PORT;
 const BOT_TOKEN = process.env.BOT_TOKEN;
+const CHAT_ID = process.env.CHAT_ID;
+const MESSAGE_ID = process.env.MESSAGE_ID;
 
 
 app.get('/tel-notif', (req, res) => {
     let message = req.query.message;//chat_id is group id and reply_to_message_id is the topic created message id
-    axios.get(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=-1001706262773&text=${message}&reply_to_message_id=6`);
+    axios.get(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=${message}&reply_to_message_id=${MESSAGE_ID}`);
     res.send('success');
 });
 
