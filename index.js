5ff60dc59620b4a19f12611d13c448ba51fff0ba
Ali Sharafi <47324261+ali-sharafi@users.noreply.github.com>
2023-06-16 20:57:16 +0000

    Complete first part

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { default: axios } = require('axios');
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
@@ -5,11 +6,13 @@ const http = require('http');
 const app = express();
 const server = http.createServer(app);
 const serverPort = process.env.SERVER_PORT;
+const BOT_TOKEN = process.env.BOT_TOKEN;
 
 
-app.get('/telegram-notif', (req, res) => {
-    // Handle the request for /telegram-notif route
-    res.send('Hello from /telegram-notif!');
+app.get('/tel-notif', (req, res) => {
+    let message = req.query.message;//chat_id is group id and reply_to_message_id is the topic created message id
+    axios.get(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=-1001706262773&text=${message}&reply_to_message_id=6`);
+    res.send('success');
 });
 
 
