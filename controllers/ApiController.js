eb6988cd551042f0d3e8e9d1ca4093bc713cb5f9
Ali Sharafi <a.sharafi1993@gmail.com>
2023-07-13 12:01:10 +0200

    Add new message type

--- controllers/ApiController.js
+++ controllers/ApiController.js
@@ -5,24 +5,32 @@ const User = require("../models/user");
 const moment = require('moment');
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const CHAT_ID = process.env.CHAT_ID;
-const OPEN_MESSAGE_ID = process.env.OPEN_MESSAGE_ID;
-const CLOSE_MESSAGE_ID = process.env.CLOSE_MESSAGE_ID;
-var lastSentTime = 0;
+const L_OPEN_MESSAGE_ID = process.env.L_OPEN_MESSAGE_ID;
+const L_CLOSE_MESSAGE_ID = process.env.L_CLOSE_MESSAGE_ID;
+const SCH_OPEN_MESSAGE_ID = process.env.SCH_OPEN_MESSAGE_ID;
+const SCH_CLOSE_MESSAGE_ID = process.env.SCH_CLOSE_MESSAGE_ID;
+var lastSentTime = {
+    '2': 0,
+    '3': 0,
+    '1': 0,
+};
 const delayThreshold = 60000;
 
 const ApiController = class {
     async sendTelNotif(req, res) {
+        let message = req.query.message;//chat_id is group id and reply_to_message_id is the topic created message id
+        let payload = req.query.payload;
+        let type = req.query.type;
         const currentTime = Date.now();
-        const timeSinceLastSent = currentTime - lastSentTime;
+        const timeSinceLastSent = currentTime - (lastSentTime[type] ?? 0);
         if (timeSinceLastSent < delayThreshold) {
             res.send('success');
             return;
         }
-        lastSentTime = currentTime;
-        let message = req.query.message;//chat_id is group id and reply_to_message_id is the topic created message id
-        let payload = req.query.payload;
-        let topic = message == 'close' ? CLOSE_MESSAGE_ID : OPEN_MESSAGE_ID;
-        axios.get(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=${message}:${payload}&reply_to_message_id=${topic}`);
+        lastSentTime[type] = currentTime;
+        let topic = this.getTopicID(type, message);
+        let telMessage = message == 'close' ? 'close' : `${message}:${payload}`;
+        axios.get(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=${telMessage}&reply_to_message_id=${topic}`);
         res.send('success');
     }
 
@@ -46,6 +54,18 @@ const ApiController = class {
             return
         }
         res.status(400).json({ message: 'We could not find your account.' });
+    }
+
+    getTopicID(type, message) {
+        if (type) {
+            if (type == '3') {//L-Topic
+                return message == 'close' ? L_CLOSE_MESSAGE_ID : L_OPEN_MESSAGE_ID;
+            } else if (type == '2') {//SCh-Topic
+                return message == 'close' ? SCH_CLOSE_MESSAGE_ID : SCH_OPEN_MESSAGE_ID;
+            }
+        }
+
+        return message == 'close' ? L_CLOSE_MESSAGE_ID : L_OPEN_MESSAGE_ID;
     }
 
     isMoreThan24Hours(updatedAt) {
